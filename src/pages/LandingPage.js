1dd049101866e7b34b3a1452ba5ef6e4afd6625f
Justin Schopick <jscho004@ucr.edu>
2018-10-24 19:14:36 -0700

    Reformatted for background and logo.

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -2,31 +2,30 @@ import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 
 class LandingPage extends Component {
-    render() {
-        return (
-            <div className="LandingPage">
-                <div>
-                    <h1>
-                        Welcome to&nbsp;
-                        <span className="blueText">G</span>
-                        <span className="redText">o</span>
-                        <span className="yellowText">o</span>
-                        <span className="blueText">g</span>
-                        <span className="greenText">l</span>
-                        <span className="redText">e</span>
-                        &nbsp;Trips
-                    </h1>
-                </div>
-
-                {/* Sign In & Sign Up Buttons */}
-                <div className="GetStarted">
-                    <NavLink to="/sign-in" className="GetStarted_Link">
-                        Click Here to Get Started
-                    </NavLink>
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="LandingPage">
+        <div className="LandingPage_Center">
+          <h1>
+            Welcome to&nbsp;
+            <span className="blueText">G</span>
+            <span className="redText">o</span>
+            <span className="yellowText">o</span>
+            <span className="blueText">g</span>
+            <span className="greenText">l</span>
+            <span className="redText">e</span>
+            &nbsp;Trips
+          </h1>
+					{/* Sign In & Sign Up Buttons */}
+					<div className="GetStarted">
+						<NavLink to="/sign-in" className="GetStarted_Link">
+							Click Here to Get Started
+						</NavLink>
+					</div>
+				</div>
+      </div>
+    );
+  }
 }
 
 export default LandingPage;
