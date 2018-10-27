6f3e076c521bc2deb1c84ea5cd651dce5345a3ed
Justin Schopick <jscho004@ucr.edu>
2018-10-27 05:15:52 -0700

    Basic Layout for the Explore Page

---
+++ src/pages/ExplorePage.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import '../styles/ExplorePage.css';
+
+class ExplorePage extends Component {
+  render() {
+    return (
+      <div className="ExplorePage">
+        <div className="Explore">
+          <h1>Explore</h1>
+        </div>
+        <div className="grid">
+            <div className="Reservations">Reservations</div>
+            <div className="ThingsToDo">Things To Do</div>
+            <div className="SavedPlaces">Saved Places</div>
+            <div className="DayPlans">Day Plans</div>
+            <div className="Discounts">Discounts</div>
+            <div className="FoodAndDrink">Food And Drink</div>
+          </div>
+      </div>
+    );
+  }
+}
+
+export default ExplorePage;
