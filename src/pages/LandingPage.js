3cdedee20f30a7f0b1cdef8c0e3a06a5922f62bf
Justin Schopick <jscho004@ucr.edu>
2018-11-14 03:52:41 -0800

    Removed Create Trip button from Landing Page

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -27,12 +27,6 @@ class LandingPage extends Component {
               Sign Up
             </Button>
           </Link>
-          &nbsp;&nbsp;&nbsp;&nbsp;
-          <Link to ="/tripsui">
-          <Button variant ="contained" color="primary" size="large">
-              Create Trip
-          </Button>
-          </Link>
         </div>
       </div>
     );
